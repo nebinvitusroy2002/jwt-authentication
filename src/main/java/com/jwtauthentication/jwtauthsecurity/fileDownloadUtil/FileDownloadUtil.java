e679687c1f71899f8ca89277e6d16d317f02d5c4
Your Name <you@example.com>
2024-12-02 17:57:42 +0530

    second commit

---
+++ src/main/java/com/jwtauthentication/jwtauthsecurity/fileDownloadUtil/FileDownloadUtil.java
@@ -0,0 +1,30 @@
+package com.jwtauthentication.jwtauthsecurity.fileDownloadUtil;
+
+
+import org.springframework.core.io.UrlResource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileDownloadUtil {
+    private Path foundFile;
+    public UrlResource getFileAsResource(String fileCode) throws IOException{
+        Path dirPath = Paths.get("Files-Upload");
+        try(Stream<Path> stream = Files.list(dirPath)) {
+            stream.forEach(file -> {
+                if (file.getFileName().toString().startsWith(fileCode)) {
+                    foundFile = file;
+                }
+            });
+        }catch (IOException e){
+            throw new IOException("Error reading files from directory: "+dirPath,e);
+        }
+        if (foundFile != null){
+            return new UrlResource(foundFile.toUri());
+        }
+        return null;
+    }
+}
